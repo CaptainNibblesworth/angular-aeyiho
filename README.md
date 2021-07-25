049f648d87e90cf87a80d41e7da065299245822c
CaptainNibblesworth <josh.innis@decentralclub.com>
2021-07-25 10:10:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-aeyiho
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-aeyiho)
---
+++ src/app/state/book.selectors.ts
@@ -0,0 +1,17 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+
+import { AppState } from './app.state';
+
+import { Book } from '../book-list/book.model';
+
+export const selectBooks = createSelector(
+  (state: AppState) => state.books,
+  (books: Array<Book>) => books
+);
+
+export const selectCollectionState = createFeatureSelector<
+  AppState,
+  ReadonlyArray<string>
+  >("collection");
+
+  
