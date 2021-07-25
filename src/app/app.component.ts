f744b9a9a6c83dc1eb5de8cd57646c57515b3e38
CaptainNibblesworth <josh.innis@decentralclub.com>
2021-07-25 12:04:19 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,8 +31,3 @@ export class AppComponent {
       .subscribe(Book => this.store.dispatch(retrievedBookList({ Book })));
   }
 }
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
